c7cb6ead4008e8d61b7927470c790a34d8d8a2f9
LitzN <alitzianedyalkova@hotmail.co.uk>
2020-09-02 22:31:59 +0000

    culture page: added slideshow for each location and linked to location selector buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,7 @@
 $(".tomar").click(function(){
   $(".location-result").html("<h1>Tomar</h1>");
+  $(".carousel").addClass("slideshow");
+  $(".tomar-slideshow").removeClass("slideshow");
   $(".location-information").html("<p>Architectural hotspot with strong religious history. Visit the Convent of Christ, 12th century monastary castle, named World Heritage Site by UNESCO. The Church of Saint Paul the Baptist is also a popular attraction, this 15th century church was built by King Manuel I.</p>")
   $(".culture-locations").css("background-color","white");
   $(".tomar").css("background-color", "blue");
@@ -7,6 +9,8 @@ $(".tomar").click(function(){
 
 $(".porto").click(function(){
   $(".location-result").html("<h1>Porto</h1>");
+  $(".carousel").addClass("slideshow");
+  $(".porto-slideshow").removeClass("slideshow");
   $(".location-information").html("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>")
   $(".culture-locations").css("background-color","white");
   $(".porto").css("background-color", "blue");
@@ -14,12 +18,16 @@ $(".porto").click(function(){
 
 $(".lisbon").click(function(){
   $(".location-result").html("<h1>Lisbon</h1>");
+  $(".carousel").addClass("slideshow");
+  $(".lisbon-slideshow").removeClass("slideshow");
   $(".culture-locations").css("background-color","white");
   $(".lisbon").css("background-color", "blue");
 });
 
 $(".braga").click(function(){
   $(".location-result").html("<h1>Braga</h1>");
+  $(".carousel").addClass("slideshow");
+  $(".braga-slideshow").removeClass("slideshow");
   $(".culture-locations").css("background-color","white");
   $(".braga").css("background-color", "blue");
 });
