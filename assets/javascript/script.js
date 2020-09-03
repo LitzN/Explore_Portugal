e460aebba6f4d6eab647a4fa9cf7553edf169da9
LitzN <alitzianedyalkova@hotmail.co.uk>
2020-09-03 22:40:54 +0000

    added some html to appear when culture location is added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,7 +11,7 @@ $(".porto").click(function(){
   $(".location-heading").html("<h1>Porto</h1>");
   $(".carousel").addClass("slideshow");
   $(".porto-slideshow").removeClass("slideshow");
-  $(".location-information").html("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>")
+  $(".location-information").html("<p>It was in the Porto and in the North regions that Portugal was founded in the 12th century and the Portuguese became a people and a nation. Porto, a World Heritage city, is the gateway and departure point for a journey across the natural and cultural diversity of the region. It is known for the Port wine which is shipped from here all over the world, but also for a heritage which combines ancient churches and monuments, such as the Cathedral and the Church of São Francisco, and modern buildings, such as Casa da Música and the Serralves Museum. </p>")
   $(".culture-locations").css("background-color","white");
   $(".porto").css("background-color", "#d6f5f5");
 });
@@ -20,6 +20,7 @@ $(".lisbon").click(function(){
   $(".location-heading").html("<h1>Lisbon</h1>");
   $(".carousel").addClass("slideshow");
   $(".lisbon-slideshow").removeClass("slideshow");
+  $(".location-information").html("<p>The wide variety of landscapes and heritage is always close by, whether to the north or south of the capital. With beaches, natural parks, cultural routes and accommodation for all tastes. Follow the river to discover some of the city’s most beautiful parts: the monumental area of Belém with its World Heritage monuments, the mediaeval quarters and the latest contemporary leisure spaces.</p>");
   $(".culture-locations").css("background-color","white");
   $(".lisbon").css("background-color", "#d6f5f5");
 });
@@ -28,6 +29,7 @@ $(".braga").click(function(){
   $(".location-heading").html("<h1>Braga</h1>");
   $(".carousel").addClass("slideshow");
   $(".braga-slideshow").removeClass("slideshow");
+  $(".location-information").html("<p>Braga is a lively city, one of the oldest in the country, and is teeming with young people who study at its universities.Every visitor to Braga must see the Bom Jesus Sanctuary, a city icon, with its monumental staircase. Amid an expanse of greenery, it offers an excellent panoramic view of the city, as do two other churches nearby: Nossa Senhora do Sameiro Sanctuary, an important place of Marian worship.</p>")
   $(".culture-locations").css("background-color","white");
   $(".braga").css("background-color", "#d6f5f5");
 });
