ca3d1b21a160242c7a3646b19d62cd1442b5bd41
LitzN <alitzianedyalkova@hotmail.co.uk>
2020-09-16 18:56:02 +0000

    Changed footer links to open in new tab, added style to culture page buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -39,8 +39,8 @@ $(".tomar").click(function(){
   $(".location-information").html("<p>Architectural hotspot with strong religious history. Visit the Convent of Christ, 12th century monastary castle, named World Heritage Site by UNESCO. The Church of Saint Paul the Baptist is also a popular attraction, this 15th century church was built by King Manuel I.</p>");
   $(".activities").addClass("hide");
   $(".activities-tomar").removeClass("hide");
-  $(".culture-locations").css("background-color","white");
-  $(".tomar").css("background-color", "#d6f5f5");
+  $(".culture-locations").removeClass("blue-shadow");
+  $(".tomar").addClass("blue-shadow");
 });
 
 $(".porto").click(function(){
@@ -50,8 +50,8 @@ $(".porto").click(function(){
   $(".location-information").html("<p>It was in the Porto and in the North regions that Portugal was founded in the 12th century and the Portuguese became a people and a nation. Porto, a World Heritage city, is the gateway and departure point for a journey across the natural and cultural diversity of the region. It is known for the Port wine which is shipped from here all over the world, but also for a heritage which combines ancient churches and monuments, such as the Cathedral and the Church of São Francisco, and modern buildings, such as Casa da Música and the Serralves Museum. </p>")
   $(".activities").addClass("hide");
   $(".activities-porto").removeClass("hide");
-  $(".culture-locations").css("background-color","white");
-  $(".porto").css("background-color", "#d6f5f5");
+  $(".culture-locations").removeClass("blue-shadow");
+  $(".porto").addClass("blue-shadow");
 });
 
 $(".lisbon").click(function(){
@@ -61,8 +61,8 @@ $(".lisbon").click(function(){
   $(".location-information").html("<p>The wide variety of landscapes and heritage is always close by, whether to the north or south of the capital. With beaches, natural parks, cultural routes and accommodation for all tastes. Follow the river to discover some of the city’s most beautiful parts: the monumental area of Belém with its World Heritage monuments, the mediaeval quarters and the latest contemporary leisure spaces.</p>");
   $(".activities").addClass("hide");
   $(".activities-lisbon").removeClass("hide");
-  $(".culture-locations").css("background-color","white");
-  $(".lisbon").css("background-color", "#d6f5f5");
+  $(".culture-locations").removeClass("blue-shadow");
+  $(".lisbon").addClass("blue-shadow");
 });
 
 $(".braga").click(function(){
@@ -72,8 +72,9 @@ $(".braga").click(function(){
   $(".location-information").html("<p>Braga is a lively city, one of the oldest in the country, and is teeming with young people who study at its universities.Every visitor to Braga must see the Bom Jesus Sanctuary, a city icon, with its monumental staircase. Amid an expanse of greenery, it offers an excellent panoramic view of the city, as do two other churches nearby: Nossa Senhora do Sameiro Sanctuary, an important place of Marian worship.</p>");
   $(".activities").addClass("hide");
   $(".activities-braga").removeClass("hide");
-  $(".culture-locations").css("background-color","white");
-  $(".braga").css("background-color", "#d6f5f5");
+  $(".culture-locations").removeClass("blue-shadow");
+  $(".braga").addClass("blue-shadow");
+
 });
 
 $(".madeira-gallery").click(function(){
